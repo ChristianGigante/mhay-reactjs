71f121730c69d9de96c951ac76c941adf0f02446
ChristianGigante <cgigante@cloudninjaphil.com>
2021-08-16 11:59:12 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="My Todos" />
+      <Todo title="My Love" />
+      <Todo title="My God" />
+    </div>
+  );
+}
+
+export default App;
